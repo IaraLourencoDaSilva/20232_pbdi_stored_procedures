24a3aaa20cc3914a86d9bd7906f1cef1ef91bd52
Iara <loure.iara.s@gmail.com>
2023-10-09 23:14:06 -0300

    ap10(stored_proc): exercicio 03

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,24 +1,44 @@
 -- Exercicio
+-- 1.3 Reescreva o exercício 1.2 de modo que o total de pedidos seja armazenado em uma
+-- variável de saída (OUT).
+CREATE OR REPLACE PROCEDURE total_pedidos_cliente(
+	IN cod_cliente INT, 
+	OUT total_pedidos INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT COUNT(*) INTO total_pedidos
+    FROM tb_pedido
+    WHERE cod_cliente = cod_cliente;
+END;
+$$;
+
+
+
+
+
+
 
 -- 1.2 Adicione um procedimento ao sistema do restaurante. Ele deve
 -- - receber um parâmetro de entrada (IN) que representa o código de um cliente
 -- - exibir, com RAISE NOTICE, o total de pedidos que o cliente tem
 
-CREATE OR REPLACE PROCEDURE sp_add_procedimento(
-	IN cod_cliente INT
-)
-LANGUAGE plpgsql
-AS $$
-DECLARE
-	total_pedidos INT;
-BEGIN 
-	SELECT COUNT(*) INTO total_pedidos
-	FROM tb_pedido
-	WHERE cod_cliente = cod_cliente;
+-- CREATE OR REPLACE PROCEDURE sp_add_procedimento(
+-- 	IN cod_cliente INT
+-- )
+-- LANGUAGE plpgsql
+-- AS $$
+-- DECLARE
+-- 	total_pedidos INT;
+-- BEGIN 
+-- 	SELECT COUNT(*) INTO total_pedidos
+-- 	FROM tb_pedido
+-- 	WHERE cod_cliente = cod_cliente;
 	
-	RAISE NOTICE 'O total de pedido é: %', total_pedidos;
-END;
-$$
+-- 	RAISE NOTICE 'O total de pedido é: %', total_pedidos;
+-- END;
+-- $$
 
 
 
